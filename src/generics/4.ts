56e76a2296a900e6738889f462a791b1a167938d
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-11-20 21:22:13 +0200

    add task 3/4 generics

---
+++ src/generics/4.ts
@@ -0,0 +1,21 @@
+type User = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser<T extends Partial<User>>(initialValues: T): User {
+  const defaultUser: User = {
+    name: "",
+    surname: "",
+    email: "",
+    password: "",
+  };
+  return { ...defaultUser, ...initialValues };
+}
+
+createOrUpdateUser({
+  email: "user@mail.com",
+  password: "password123",
+});
